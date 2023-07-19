06376f703cfe4ebf95f03703ae7ba14688140202
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-07-19 03:11:51 -0300

    iExpense

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -15,21 +15,34 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                Section("Type of Expenses") {
-                    Picker("Type of Expenses", selection: $typeOfExpenses) {
-                        ForEach(TypeOfExpenses.allCases, id: \.self) {
-                            Text($0.rawValue)
+//                Section("Type of Expenses") {
+//                    Picker("Type of Expenses", selection: $typeOfExpenses) {
+//                        ForEach(TypeOfExpenses.allCases, id: \.self) {
+//                            Text($0.rawValue)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                }
+//                .listRowBackground(Color(CGColor(red: 240, green: 240, blue: 246, alpha: 0)))
+                if !expenses.items.isEmpty {
+                    Section("Expenses:") {
+                        ForEach(expenses.items) { item in
+                            HStack {
+                                VStack {
+                                    Text(item.name)
+                                        .font(.headline.bold())
+                                    Text(item.type)
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            }
                         }
+                        .onDelete(perform: removeRows)
                     }
-                    .pickerStyle(.segmented)
-                }
-                .listRowBackground(Color(CGColor(red: 240, green: 240, blue: 246, alpha: 0)))
-                
-                Section("Expenses:") {
-                    ForEach(expenses.items) { item in
-                        Text(item.name)
-                    }
-                    .onDelete(perform: removeRows)
                 }
             }
             .navigationTitle("iExpense")
@@ -45,21 +58,21 @@ struct ContentView: View {
                     EditButton()
                 }
             }
-            .toolbar {
-                ToolbarItem(placement: .bottomBar) {
-                    VStack {
-                        Text("Total \(typeOfExpenses.rawValue) Expenses: 300")
-                            .font(.subheadline.bold())
-                            .foregroundColor(.gray)
-                        
-                        Spacer()
-                        
-                        Text("Total Expenses: 300")
-                            .font(.subheadline.bold())
-                            .foregroundColor(.gray)
-                    }
-                }
-            }
+//            .toolbar {
+//                ToolbarItem(placement: .bottomBar) {
+//                    VStack {
+//                        Text("Total \(typeOfExpenses.rawValue) Expenses: 300")
+//                            .font(.subheadline.bold())
+//                            .foregroundColor(.gray)
+//
+//                        Spacer()
+//
+//                        Text("Total Expenses: 300")
+//                            .font(.subheadline.bold())
+//                            .foregroundColor(.gray)
+//                    }
+//                }
+//            }
             .sheet(isPresented: $showingExpenses) {
                 AddExpenseView(expenses: expenses)
             }
