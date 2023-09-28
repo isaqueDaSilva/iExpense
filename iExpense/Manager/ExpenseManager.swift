ce81497e82de1bba978f66a524fa1aab25fd04b4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-09-28 00:16:39 -0300

    App updated for the MVVM architecture.

--- iExpense/Manager/ExpenseManager.swift
+++ iExpense/Manager/ExpenseManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+actor ExpenseManager {
+    static let shared = ExpenseManager()
+    
+    var items = [ExpensesItems]() {
+        didSet {
+            if let encoderItems = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoderItems, forKey: "Items")
+            }
+        }
+    }
+    
+    func appendItens(name: String, type: TypeOfExpenses, date: Date, amount: Double) {
+        let item = ExpensesItems(name: name, type: type.rawValue, date: date, amount: amount)
+        items.append(item)
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
+    
+    private init() { 
+        if let saveItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decoderItems = try? JSONDecoder().decode([ExpensesItems].self, from: saveItems) {
+                items = decoderItems
+                return
+            }
+        }
+        items = []
+    }
+}
--- iExpense/UI Components/ListView.swift
+++ iExpense/UI Components/ListView.swift
@@ -5,4 +5,18 @@
 //  Created by Isaque da Silva on 27/09/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ListView: View {
+    var title: String
+    var description: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            Spacer()
+            Text(description)
+                .foregroundColor(.gray)
+        }
+    }
+}
